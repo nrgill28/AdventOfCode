eb57a38f0da1fa1ab777efabe36e14bbcd7ee1cc
Nathan Gill <nrgill28@gmail.com>
2020-12-06 05:16:15 -0500

    2020 Day 6

---
+++ AdventOfCode/Solutions/2020/Days/Day6.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+namespace AdventOfCode
+{
+    [AoCSolution(Year = 2020, Day = 6, Name = "Custom Customs")]
+    public class AoC2020Day6 : AoCSolution
+    {
+        //protected override string DebugInput => "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb";
+
+        public override object RunPart1()
+        {
+            return Input.Split("\n\n").Aggregate(0, (sum, group) =>
+            {
+                var questions = new bool[26];
+                
+                foreach (var person in group.Split("\n").Where(l => !string.IsNullOrWhiteSpace(l)))
+                foreach (var answer in person)
+                    questions[answer - 97] = true;
+                
+                return sum + questions.Count(x => x);
+            });
+        }
+
+        public override object RunPart2()
+        {
+            return Input.Split("\n\n").Aggregate(0, (sum, group) =>
+            {
+                var questions = new int[26];
+                var people = group.Split("\n").Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+                
+                foreach (var person in people)
+                foreach (var answer in person)
+                    questions[answer - 97] += 1;
+                
+                return sum + questions.Count(x => x == people.Length);
+            });
+        }
+    }
+}
