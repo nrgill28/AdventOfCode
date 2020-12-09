567433161d7571fd1bebf49801f1872080317e14
Nathan Gill <nrgill28@gmail.com>
2020-12-09 03:04:02 -0500

    Some docs

--- AdventOfCode/Solutions/2020/Days/Day9.cs
+++ AdventOfCode/Solutions/2020/Days/Day9.cs
@@ -11,21 +11,28 @@ namespace AdventOfCode
 
         public override object RunPart1()
         {
+            // Declare a new list
             var list = new List<long>();
+            
+            // Start iterating over the numbers in the input
             foreach (var num in InputAsLongs())
             {
+                // If we haven't yet filled our list add the number and skip to the next iteration
                 if (list.Count < PreambleLength)
                 {
                     list.Add(num);
                     continue;
                 }
 
+                // If the list doesn't contain to distinct numbers that sum to the current number
+                // we found our value. Return it
                 if (!DoesListHaveSum(list, num))
                 {
                     _part1 = num;
                     return num;
                 }
 
+                // Otherwise remove the first element and add the current number
                 list.RemoveAt(0);
                 list.Add(num);
             }
@@ -35,18 +42,27 @@ namespace AdventOfCode
 
         public override object RunPart2()
         {
+            // Get our input
             var input = InputAsLongs();
+            
+            // For each number in the input
             for (var i = 0; i < input.Length - 1; i++)
             {
+                // Start summing the numbers that come directly after it
                 var sum = input[i];
                 for (var j = i + 1; j < input.Length; j++)
                 {
                     sum += input[j];
+                    
+                    // If we reached the target value;
                     if (sum == _part1)
                     {
+                        // Get the range and return the min plus the max
                         var range = input.Skip(i).Take(j - i).ToArray();
                         return range.Min() + range.Max();
                     } 
+                    
+                    // If we overshot the target value skip to the next loop
                     if (sum > _part1) break;
                 }
             }
@@ -56,6 +72,7 @@ namespace AdventOfCode
 
         private static bool DoesListHaveSum(IList<long> list, long sum)
         {
+            // Checks if two distinct numbers in the given list sum to the given value
             for (var i = 0; i < list.Count - 1; i++)
             for (var j = i + 1; j < list.Count; j++)
                 if (list[i] + list[j] == sum)
